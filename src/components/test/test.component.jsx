51f093b6b2e76684867f51795c7b0d2fb3777e96
rahdi <adrian.heiden@gmail.com>
2021-04-07 13:10:45 +0200

    added width of window

--- src/components/test/test.component.jsx
+++ src/components/test/test.component.jsx
@@ -4,6 +4,7 @@ import "./test.styles.scss";
 
 export class TestComponent extends Component {
   render() {
+    const width = window.innerWidth;
     return (
       <div className="test-grid">
         <div className="grid-item">Element 1</div>
@@ -16,6 +17,7 @@ export class TestComponent extends Component {
         <div className="grid-item">Element 1</div>
         <div className="grid-item">Element 1</div>
         <div className="grid-item">Element 1</div>
+        <div className="grid-item">{width}</div>
       </div>
     );
   }
