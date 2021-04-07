95e40b44380d97ad8780ea436772e67f63a610cd
rahdi <adrian.heiden@gmail.com>
2021-04-07 12:47:28 +0200

    testing something

---
+++ src/components/test/test.component.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import "./test.styles.scss";
+
+export class TestComponent extends Component {
+  render() {
+    return (
+      <div className="test-grid">
+        <div className="grid-item">Element 1</div>
+        <div className="grid-item">Element 1</div>
+        <div className="grid-item">Element 1</div>
+        <div className="grid-item">Element 1</div>
+        <div className="grid-item">Element 1</div>
+        <div className="grid-item">Element 1</div>
+        <div className="grid-item">Element 1</div>
+        <div className="grid-item">Element 1</div>
+        <div className="grid-item">Element 1</div>
+        <div className="grid-item">Element 1</div>
+      </div>
+    );
+  }
+}
+
+export default TestComponent;
